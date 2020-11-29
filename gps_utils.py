02e6c3b16ca69443d880c915d9612e314d88cf79
crclz <crucialize@foxmail.com>
2020-11-29 09:59:28 +0800

    Add distance function

--- gps_utils.py
+++ gps_utils.py
@@ -124,3 +124,22 @@ def out_of_china(lng, lat):
     :return:
     """
     return not (lng > 73.66 and lng < 135.05 and lat > 3.86 and lat < 53.55)
+
+
+def distance(p1, p2):
+    lon1, lat1 = p1
+    lon2, lat2 = p2
+
+    R = 6371e3
+    φ1 = lat1 * math.pi/180
+    φ2 = lat2 * math.pi/180
+    Δφ = (lat2-lat1) * math.pi/180
+    Δλ = (lon2-lon1) * math.pi/180
+
+    a = math.sin(Δφ/2) * math.sin(Δφ/2) + math.cos(φ1) * \
+        math.cos(φ2) * math.sin(Δλ/2) * math.sin(Δλ/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+
+    d = R * c
+
+    return d
